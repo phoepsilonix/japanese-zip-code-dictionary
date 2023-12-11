b6fbb5169e4da9f5636d21ca6f3d532552a2bdce
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-12-12 00:38:00 +0900

    hinshi
    KEN_ALL
    BOS/EOS => 地名
    
    JIGYOSYO
    BOS/EOS => 組織

--- scripts/make.sh
+++ scripts/make.sh
@@ -3,7 +3,8 @@
 wget https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip
 wget https://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip
 
-SRCTXT=ZIP.CSV
+SEEDTXT1=ZIP.CSV
+SEEDTXT2=ZIP2.CSV
 SRCTXT1=KEN_ALL.CSV
 SRCTXT2=JIGYOSYO.CSV
 USERDIC=user_dic-japanese-zip-code
@@ -11,16 +12,21 @@ USERDIC=user_dic-japanese-zip-code
 unzip ken_all.zip
 unzip jigyosyo.zip
 
-PYTHONPATH="$PWD:$PYTHONPATH" python dictionary/gen_zip_code_seed.py --zip_code="KEN_ALL.CSV" --jigyosyo="JIGYOSYO.CSV" > ${SRCTXT}
+PYTHONPATH="$PWD:$PYTHONPATH" python dictionary/gen_zip_code_seed.py --zip_code="${SRCTXT1}" > ${SEEDTXT1}
+PYTHONPATH="$PWD:$PYTHONPATH" python dictionary/gen_zip_code_seed.py --jigyosyo="${SRCTXT2}" > ${SEEDTXT2}
 
-ruby user_dict.rb -i user_dic_id.def -f ${SRCTXT} > ../${USERDIC}
+ruby user_dict.rb -i user_dic_id.def -f ${SEEDTXT1} > ../${USERDIC}
+ruby user_dict.rb -i user_dic_id.def -f ${SEEDTXT2} > ../${USERDIC}2
+sed 's|BOS/EOS|地名|' -i ../${USERDIC}
+sed 's|BOS/EOS|組織|' -i ../${USERDIC}2
+cat ../${USERDIC}2 >> ../${USERDIC}
 
 split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt ../$USERDIC $USERDIC-
-rm ../$USERDIC
+rm ../${USERDIC} ../${USERDIC}2
 
 [[ -e ../release/${USERDIC}.tar.xz ]] && rm ../release/${USERDIC}.tar.xz
-
+mkdir -p ../release
 tar cf ../release/${USERDIC}.tar ${USERDIC}-*.txt ../LICENSE.user_dic
 xz -9 -e ../release/${USERDIC}.tar
 
-rm $SRCTXT $SRCTXT1 $SRCTXT2 ken_all.zip jigyosyo.zip $USERDIC-*.txt
+rm $SEEDTXT $SEEDTXT2 $SRCTXT1 $SRCTXT2 ken_all.zip jigyosyo.zip $USERDIC-*.txt
