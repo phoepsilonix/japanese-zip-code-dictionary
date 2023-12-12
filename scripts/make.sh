feaaad5923f7053bc84bd20d2ae33ceb33e478e6
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-12-12 19:54:07 +0900

    removing depends on ruby scripts

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip
-wget https://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip
+wget -nc https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip
+wget -nc https://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip
 
 SEEDTXT1=ZIP.CSV
 SEEDTXT2=ZIP2.CSV
@@ -9,20 +9,17 @@ SRCTXT1=KEN_ALL.CSV
 SRCTXT2=JIGYOSYO.CSV
 USERDIC=user_dic-japanese-zip-code
 
-unzip ken_all.zip
-unzip jigyosyo.zip
+unzip -o ken_all.zip
+unzip -o jigyosyo.zip
 
 PYTHONPATH="$PWD:$PYTHONPATH" python dictionary/gen_zip_code_seed.py --zip_code="${SRCTXT1}" > ${SEEDTXT1}
 PYTHONPATH="$PWD:$PYTHONPATH" python dictionary/gen_zip_code_seed.py --jigyosyo="${SRCTXT2}" > ${SEEDTXT2}
 
-ruby user_dict.rb -i user_dic_id.def -f ${SEEDTXT1} > ../${USERDIC}
-ruby user_dict.rb -i user_dic_id.def -f ${SEEDTXT2} > ../${USERDIC}2
-sed 's|BOS/EOS|地名|' -i ../${USERDIC}
-sed 's|BOS/EOS|組織|' -i ../${USERDIC}2
-cat ../${USERDIC}2 >> ../${USERDIC}
+cat ${SEEDTXT1} |awk 'BEGIN{FS="\t"}{print $1 "\t" $5 "\t" "地名" "\t"}' |sort -u > ../${USERDIC}
+cat ${SEEDTXT2} |awk 'BEGIN{FS="\t"}{print $1 "\t" $5 "\t" "組織" "\t"}' |sort -u >> ../${USERDIC}
 
 split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt ../$USERDIC $USERDIC-
-rm ../${USERDIC} ../${USERDIC}2
+rm ../${USERDIC}
 
 [[ -e ../release/${USERDIC}.tar.xz ]] && rm ../release/${USERDIC}.tar.xz
 mkdir -p ../release
